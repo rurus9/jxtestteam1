1d3d8cd8e9935b300b6878034b31ded2c202ba2e
Grzegorz Patron <grzegorz.patron@assecobs.pl>
2019-07-05 13:39:51 +0200

    initial change1

--- src/index.php
+++ src/index.php
@@ -2,6 +2,7 @@
 
 echo "Hello, World from Kubernetes! <br>";
 echo "Hello Jenkins X! <br>";
+echo "Zmiana 1! <br>";
 echo '<img border="0" src="https://jenkins-x.io/images/logo.png" width="300" height="300" />';
 
 ?>
